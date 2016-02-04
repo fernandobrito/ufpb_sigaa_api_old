70bd7d9ac12351e74662848a57d91b23fb9ecdb1
Fernando Brito <email@fernandobrito.com>
2016-02-04 14:53:21 -0600

    Add /api/curricula data for testing front-end

--- main.rb
+++ main.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'sinatra/namespace'
+require 'json'
 
 get '/' do
   "Hello World!"
+end
+
+namespace '/api' do
+  get '/curricula' do
+    content_type :json
+
+    File.read('data/curricula.json')
+  end
 end
