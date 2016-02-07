276e360f76a61cd519a8576c8450e5d232fec809
Fernando Brito <email@fernandobrito.com>
2016-02-07 08:44:39 -0600

    Add cross origin support

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'sinatra-cross_origin', '~> 0.3.1'
--- main.rb
+++ main.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'sinatra/namespace'
-require 'json'
+require 'sinatra/cross_origin'
+
+configure do
+  enable :cross_origin
+end
 
 get '/' do
   "Hello World!"
